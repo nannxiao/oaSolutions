57e0f99b8e295da70c07792c2f344e47035f4964
nannxiao <nanxiao@uw.edu>
2017-02-25 21:33:50 -0800

    Update duplicate.py

--- quantcast/duplicate.py
+++ quantcast/duplicate.py
@@ -1,4 +1,4 @@
-    def containsDuplicate(self, nums, k):
+    def containsDuplicate(nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -14,3 +14,30 @@
                     if nums[m] == n:
                         list.append(m)
         return list
+    
+    
+    
+# O(n)  
+
+    def containsDuplicate(s, k):
+        dic = defaultdict(list)
+        list = []
+        
+    # set every character as key in dictionary
+    # then add a list to every key containing the corresponding indexes   
+    
+        for i, val in enumerate(s):
+            dic[val].append(i)
+            
+    # check if the length of every list is greater than k 
+    # if is, add every index of the list to our outcome list
+    
+        for key in dic.keys():
+            if len(dic[key]) >= k:
+                for num in dic[key]:
+                    list.append(num)
+        return list
+        
+        
+        
+        
