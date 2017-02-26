befff8b48297bcb4b2b266dd93485120ed4897c5
nannxiao <nanxiao@uw.edu>
2017-02-26 01:02:11 -0800

    Update averageDepthofLeaves.py

--- quantcast/averageDepthofLeaves.py
+++ quantcast/averageDepthofLeaves.py
@@ -60,7 +60,23 @@ def averageLeavesDepth(root):
         return res
 # a function to sum depth of all leaves:
 
-
+def maxDepth(self, root):     
+     if not root:
+         return 0
+     
+     tqueue, h = collections.deque(),0
+     tqueue.append(root)
+     while tqueue:
+         nextlevel = collections.deque()
+         while tqueue:
+             front = tqueue.popleft()
+             if front.left:
+                 nextlevel.append(front.left)
+             if front.right:
+                 nextlevel.append(front.right)
+         tqueue = nextlevel
+         h += 1
+     return h
 
 
 
