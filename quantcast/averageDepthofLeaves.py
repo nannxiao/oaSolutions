025b3d761cd42b7c96fd91b495a499265507bf57
nannxiao <nanxiao@uw.edu>
2017-02-26 00:28:06 -0800

    Update averageDepthofLeaves.py

--- quantcast/averageDepthofLeaves.py
+++ quantcast/averageDepthofLeaves.py
@@ -1,3 +1,4 @@
+# recursive
 # a function to count the number of leaves in a given BST:
 def countLeaves(root):
     # return 0 when the node was null:
@@ -31,7 +32,43 @@ def sumLeavesDepth(root, d):
 def averageLeavesDepth(root):
     print "the average depth of all leaves is:", sumLeavesDepth(root, 0)/countLeaves(root)
 
+    
+    
+# iterative  
+# a function to count all the leaves of given BST:
+    def countLeaves(root):
+        if not root: 
+            return 0
+        
+        s = []
+        s.append(root)
+        res = 0
+        
+        while s:
+            tmp = s.pop()
+            
+            if tmp.left:
+                s.append(tmp.left)
+                if not tmp.left.left and not tmp.left.right:
+                    res += 1
+                    
+            if tmp.right:
+                s.append(tmp.right)
+                if not tmp.right.left and not tmp.right.right:
+                    res += 1
+                
+        return res
+# a function to sum depth of all leaves:
 
 
+
+
+
+
+
+
+
+
+        
 
 
